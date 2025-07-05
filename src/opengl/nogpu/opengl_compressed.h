54be66ed1b6302894cd17ad9f077fb5afe238203
Cristian Camilo Ruiz <mrgaturus@gmail.com>
2025-07-05 04:48:49 -0500

    opengl: declare compressed texture objects

---
+++ src/opengl/nogpu/opengl_compressed.h
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Cristian Camilo Ruiz <mrgaturus>
+#ifndef OPENGL_COMPRESSED_H
+#define OPENGL_COMPRESSED_H
+#include <nogpu/compressed.h>
+#include "opengl_texture.h"
+
+class GLCompressed1D : GLTexture, GPUCompressed1D {
+    // Texture Buffer Manipulation
+    void allocate(int size, int levels) override;
+    void upload(int x, int size, int level, void* data) override;
+    void unpack(int x, int size, int level, GPUBuffer *pbo, int offset) override;
+
+    protected: GLCompressed1D(
+        GLContext* ctx,
+        GPUTexturePixelType type,
+        GPUTexturePixelFormat format);
+        friend GLContext;
+};
+
+class GLCompressed2D : GLTexture, GPUCompressed2D {
+    GPUTexture2DMode getMode() override;
+    void setMode(GPUTexture2DMode mode);
+
+    // Texture Buffer Manipulation
+    void allocate(GPUTexture2DMode mode, int w, int h, int levels) override;
+    void upload(int x, int y, int w, int h, int level, void* data) override;
+    void unpack(int x, int y, int w, int h, int level, GPUBuffer *pbo, int offset) override;
+
+    protected: GLCompressed2D(
+        GLContext* ctx,
+        GPUTexturePixelType type,
+        GPUTexturePixelFormat format);
+        friend GLContext;
+};
+
+class GLCompressed3D : GLTexture, GPUCompressed3D {
+    GPUTexture3DMode getMode() override;
+    void setMode(GPUTexture3DMode mode);
+
+    // Texture Buffer Manipulation
+    void allocate(GPUTexture3DMode mode, int w, int h, int depth, int levels) override;
+    void upload(int x, int y, int z, int w, int h, int depth, int level, void* data) override;
+    void unpack(int x, int y, int z, int w, int h, int depth, int level, GPUBuffer *pbo, int offset) override;
+
+    protected: GLCompressed3D(
+        GLContext* ctx,
+        GPUTexturePixelType type,
+        GPUTexturePixelFormat format);
+        friend GLContext;
+};
+
+class GLCompressedCubemap : GLTexture, GPUCompressedCubemap {
+    // Texture Buffer Manipulation
+    void allocate(int w, int h, int levels) override;
+    void upload(GPUTextureCubemapSide side, int x, int y, int w, int h, int level, void* data) override;
+    void unpack(GPUTextureCubemapSide side, int x, int y, int w, int h, int level, GPUBuffer *pbo, int offset) override;
+
+    protected: GLCompressedCubemap(
+        GLContext* ctx,
+        GPUTexturePixelType type,
+        GPUTexturePixelFormat format);
+        friend GLContext;
+};
+
+class GLCompressedCubemapArray : GLTexture, GPUCompressedCubemapArray {
+    // Texture Buffer Manipulation
+    void allocate(int w, int h, int layers, int levels) override;
+    void upload(GPUTextureCubemapSide side, int x, int y, int w, int h, int layer, int level, void* data) override;
+    void unpack(GPUTextureCubemapSide side, int x, int y, int w, int h, int layer, int level, GPUBuffer *pbo, int offset) override;
+
+    protected: GLCompressedCubemapArray(
+        GLContext* ctx,
+        GPUTexturePixelType type,
+        GPUTexturePixelFormat format);
+        friend GLContext;
+};
+
+#endif // OPENGL_COMPRESSED_H
