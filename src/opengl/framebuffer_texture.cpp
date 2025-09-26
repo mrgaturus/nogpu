04124c8d76f04ea81e7d952130883783566d9d0e
Cristian Camilo Ruiz <mrgaturus@gmail.com>
2025-09-26 13:15:13 -0500

    opengl: misc framebuffer texture code change

--- src/opengl/framebuffer_texture.cpp
+++ src/opengl/framebuffer_texture.cpp
@@ -5,114 +5,6 @@
 #include "private/context.h"
 #include "private/glad.h"
 
-// -------------------------------
-// OpenGL RenderBuffer: Attachment
-// -------------------------------
-
-static GPUFrameBufferStatus toValue(GLenum status) {
-    switch (status) {
-        default:
-            return GPUFrameBufferStatus::FRAMEBUFFER_UNSUPPORTED;
-        case GL_FRAMEBUFFER_UNDEFINED:
-            return GPUFrameBufferStatus::FRAMEBUFFER_UNDEFINED;
-
-        // Incomplete Framebuffer Errors
-        case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
-            return GPUFrameBufferStatus::FRAMEBUFFER_INCOMPLETE_ATTACHMENT;
-        case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
-            return GPUFrameBufferStatus::FRAMEBUFFER_INCOMPLETE_MULTISAMPLE;
-        case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
-            return GPUFrameBufferStatus::FRAMEBUFFER_INCOMPLETE_MISSING;
-    }
-}
-
-void GLFrameBuffer::updateAttachment(GLenum attachment, GLRenderLink* link) {
-    GLRenderBuffer* target = link->target;
-    GLint layer = link->slice.layer;
-    GLint level = link->slice.level;
-
-    // Check Attachment Existence
-    if (target == nullptr) {
-        if (link->tex_cache != 0) {
-            glFramebufferTexture2D(GL_FRAMEBUFFER,
-                attachment, GL_TEXTURE_2D, 0, 0);
-            // Remove Cache
-            link->tex_cache = 0;
-            return;
-        }
-    } else if (link->tex_cache == target->m_tex)
-        return; // Skip Cached Attachments
-
-    target->updateExternal();
-    GLuint tex = target->m_tex;
-    link->tex_cache = tex;
-    // Update OpenGL Object
-    switch (target->m_mode) {
-        case GPURenderBufferMode::RENDERBUFFER_UNDEFINED:
-            glFramebufferTexture2D(GL_FRAMEBUFFER,
-                attachment, GL_TEXTURE_2D, 0, 0);
-            GPUReport::warning("attached undefined renderbuffer");
-            break;
-
-        // Offscreen Rendering
-        case GPURenderBufferMode::RENDERBUFFER_OFFSCREEN:
-            glFramebufferRenderbuffer(GL_FRAMEBUFFER,
-                attachment, GL_RENDERBUFFER, tex);
-            break;
-        
-        // Framebuffer 1D Texture
-        case GPURenderBufferMode::RENDERBUFFER_TARGET_1D:
-            glFramebufferTexture1D(GL_FRAMEBUFFER,
-                attachment, target->m_tex_target, tex, level);
-            break;
-
-        // Framebuffer 2D Texture
-        case GPURenderBufferMode::RENDERBUFFER_TEXTURE_2D:
-        case GPURenderBufferMode::RENDERBUFFER_TEXTURE_MULTISAMPLE_2D:
-        case GPURenderBufferMode::RENDERBUFFER_TARGET_2D:
-            glFramebufferTexture2D(GL_FRAMEBUFFER,
-                attachment, target->m_tex_target, tex, level);
-            break;
-
-        // Framebuffer 2D Array, 3D Textures
-        case GPURenderBufferMode::RENDERBUFFER_TEXTURE_3D:
-        case GPURenderBufferMode::RENDERBUFFER_TEXTURE_ARRAY:
-        case GPURenderBufferMode::RENDERBUFFER_TEXTURE_MULTISAMPLE_ARRAY:
-        case GPURenderBufferMode::RENDERBUFFER_TARGET_3D:
-        case GPURenderBufferMode::RENDERBUFFER_TARGET_ARRAY:
-        case GPURenderBufferMode::RENDERBUFFER_TARGET_CUBEMAP:
-        case GPURenderBufferMode::RENDERBUFFER_TARGET_CUBEMAP_ARRAY:
-            glFramebufferTextureLayer(GL_FRAMEBUFFER,
-                attachment, tex, level, layer);
-            break;
-    }
-}
-
-GPUFrameBufferStatus GLFrameBuffer::checkAttachments() {
-    m_ctx->makeCurrentTexture(this);
-    glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
-
-    // Check Color Attachments
-    GLRenderIndexes* list = &m_colors_index;
-    for (int i = 0; i < list->count; i++) {
-        GLRenderLink* link = m_colors_index.links[i];
-        GLuint index = m_colors_index.indexes[i];
-        this->updateAttachment(GL_COLOR_ATTACHMENT0 + index, link);
-    }
-
-    // Update Stencil and Depth Attachment
-    this->updateAttachment(GL_DEPTH_ATTACHMENT, &m_depth);
-    this->updateAttachment(GL_STENCIL_ATTACHMENT, &m_stencil);
-    // Check Framebuffer and Return
-    GLenum check = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-    return toValue(check);
-}
-
-// -----------------------------------------------
-// OpenGL Renderbuffer: Internal Texture Offscreen
-// -----------------------------------------------
-
 void GLRenderBuffer::createOffscreen(int w, int h, int samples) {
     m_ctx->makeCurrentTexture(this);
     this->destroyInternal();
@@ -254,4 +146,108 @@ void GLRenderBuffer::createTexture3D(int w, int h, int layers, int levels) {
     m_target->m_height = h;
     m_target->m_depth = layers;
     m_samples = 1;
+}
+
+// ------------------------------
+// OpenGL FrameBuffer: Attachment
+// ------------------------------
+
+static GPUFrameBufferStatus toValue(GLenum status) {
+    switch (status) {
+        default:
+            return GPUFrameBufferStatus::FRAMEBUFFER_UNSUPPORTED;
+        case GL_FRAMEBUFFER_UNDEFINED:
+            return GPUFrameBufferStatus::FRAMEBUFFER_UNDEFINED;
+
+        // Incomplete Framebuffer Errors
+        case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
+            return GPUFrameBufferStatus::FRAMEBUFFER_INCOMPLETE_ATTACHMENT;
+        case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
+            return GPUFrameBufferStatus::FRAMEBUFFER_INCOMPLETE_MULTISAMPLE;
+        case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
+            return GPUFrameBufferStatus::FRAMEBUFFER_INCOMPLETE_MISSING;
+    }
+}
+
+void GLFrameBuffer::updateAttachment(GLenum attachment, GLRenderLink* link) {
+    GLRenderBuffer* target = link->target;
+    GLint layer = link->slice.layer;
+    GLint level = link->slice.level;
+
+    // Check Attachment Existence
+    if (target == nullptr) {
+        if (link->tex_cache != 0) {
+            glFramebufferTexture2D(GL_FRAMEBUFFER,
+                attachment, GL_TEXTURE_2D, 0, 0);
+            // Remove Cache
+            link->tex_cache = 0;
+            return;
+        }
+    } else if (link->tex_cache == target->m_tex)
+        return; // Skip Cached Attachments
+
+    target->updateExternal();
+    GLuint tex = target->m_tex;
+    link->tex_cache = tex;
+    // Update OpenGL Object
+    switch (target->m_mode) {
+        case GPURenderBufferMode::RENDERBUFFER_UNDEFINED:
+            glFramebufferTexture2D(GL_FRAMEBUFFER,
+                attachment, GL_TEXTURE_2D, 0, 0);
+            GPUReport::warning("attached undefined renderbuffer");
+            break;
+
+        // Offscreen Rendering
+        case GPURenderBufferMode::RENDERBUFFER_OFFSCREEN:
+            glFramebufferRenderbuffer(GL_FRAMEBUFFER,
+                attachment, GL_RENDERBUFFER, tex);
+            break;
+        
+        // Framebuffer 1D Texture
+        case GPURenderBufferMode::RENDERBUFFER_TARGET_1D:
+            glFramebufferTexture1D(GL_FRAMEBUFFER,
+                attachment, target->m_tex_target, tex, level);
+            break;
+
+        // Framebuffer 2D Texture
+        case GPURenderBufferMode::RENDERBUFFER_TEXTURE_2D:
+        case GPURenderBufferMode::RENDERBUFFER_TEXTURE_MULTISAMPLE_2D:
+        case GPURenderBufferMode::RENDERBUFFER_TARGET_2D:
+            glFramebufferTexture2D(GL_FRAMEBUFFER,
+                attachment, target->m_tex_target, tex, level);
+            break;
+
+        // Framebuffer 2D Array, 3D Textures
+        case GPURenderBufferMode::RENDERBUFFER_TEXTURE_3D:
+        case GPURenderBufferMode::RENDERBUFFER_TEXTURE_ARRAY:
+        case GPURenderBufferMode::RENDERBUFFER_TEXTURE_MULTISAMPLE_ARRAY:
+        case GPURenderBufferMode::RENDERBUFFER_TARGET_3D:
+        case GPURenderBufferMode::RENDERBUFFER_TARGET_ARRAY:
+        case GPURenderBufferMode::RENDERBUFFER_TARGET_CUBEMAP:
+        case GPURenderBufferMode::RENDERBUFFER_TARGET_CUBEMAP_ARRAY:
+            glFramebufferTextureLayer(GL_FRAMEBUFFER,
+                attachment, tex, level, layer);
+            break;
+    }
+}
+
+GPUFrameBufferStatus GLFrameBuffer::checkAttachments() {
+    m_ctx->makeCurrentTexture(this);
+    glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
+
+    // Check Color Attachments
+    GLRenderIndexes* list = &m_colors_index;
+    for (int i = 0; i < list->count; i++) {
+        GLRenderLink* link = m_colors_index.links[i];
+        GLuint index = m_colors_index.indexes[i];
+        this->updateAttachment(GL_COLOR_ATTACHMENT0 + index, link);
+    }
+
+    // Update Stencil and Depth Attachment
+    this->updateAttachment(GL_DEPTH_ATTACHMENT, &m_depth);
+    this->updateAttachment(GL_STENCIL_ATTACHMENT, &m_stencil);
+    // Check Framebuffer and Return
+    GLenum check = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    return toValue(check);
 }
